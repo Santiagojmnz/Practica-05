729d51bb4cb20c3dc33fa5f26eae8141b19c1c75
Luis Franco <josefranco.cbtis16@gmail.com>
2022-06-06 21:51:26 -0500

    Corrigiendo rama corrupta

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 
 // cargar rutas
-var estado_routers = require('./routes/estado');
+// var estado_routers = require('./routes/estado');
+var localidad_routers = require('./routes/localidad');
 //Servidor
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,6 +16,7 @@ app.use(cors());
 app.options('*', cors());
 
 //Rutas base
-app.use('/api', estado_routers);
+// app.use('/api', estado_routers);
+app.use('/api', localidad_routers);
 
 module.exports = app;
